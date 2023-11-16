5d82753b5e662c62a99a049fd7b252e0b3b7b0eb
Talha Waris <talhawaris39@gmail.com>
2023-11-16 17:09:36 +0500

    Add Acknowledgment feature

--- app/src/main/java/com/devdroiddev/googleinab/MainActivity.kt
+++ app/src/main/java/com/devdroiddev/googleinab/MainActivity.kt
@@ -3,96 +3,70 @@ package com.devdroiddev.googleinab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.android.billingclient.api.AcknowledgePurchaseParams
+import com.android.billingclient.api.AcknowledgePurchaseResponseListener
 import com.android.billingclient.api.BillingClient
 import com.android.billingclient.api.BillingClient.BillingResponseCode
 import com.android.billingclient.api.BillingClientStateListener
 import com.android.billingclient.api.BillingFlowParams
 import com.android.billingclient.api.BillingResult
 import com.android.billingclient.api.ProductDetails
+import com.android.billingclient.api.Purchase
+import com.android.billingclient.api.Purchase.PurchaseState
 import com.android.billingclient.api.PurchasesUpdatedListener
 import com.android.billingclient.api.QueryProductDetailsParams
 import com.android.billingclient.api.SkuDetailsParams
 import com.devdroiddev.googleinab.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PurchasesUpdatedListener, BillingClientStateListener {
 
-    private lateinit var binding : ActivityMainBinding
-    private var billingClient : BillingClient? = null
-    private lateinit var skuList: ArrayList<String>
+    companion object {
+        const val APP_TAG = "Billing_App"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+    private var billingClient: BillingClient? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // add products to arraylist
-        skuList = ArrayList()
-        skuList.add("android.test.purchased") // for testing purpose
         init()
-        //setUpListeners()
+        initListeners()
     }
 
     private fun init() {
 
-        // Initialize PurchaseUpdateListener
-      /*  val purchaseUpdateListener = PurchasesUpdatedListener{
-            billingResult, purchases ->
-        }
-
-        // Initialize Billing Client
-        billingClient = BillingClient.newBuilder(this)  // pass any context Billing client take it as application context
-            .setListener(purchaseUpdateListener)  // pass a reference of purchaseUpdateListener to setListener() to received updates on purchases
-            .enablePendingPurchases()
-            .build()*/
-
         // Initialize Billing Client
         initializeBillingClient()
+        //start the connection after initializing the billing client
+        establishedConnection()
+    }
+
+    private fun initListeners() {
+
     }
 
     private fun initializeBillingClient() {
 
-        // Purchase Update Listener
-        val purchaseUpdateListener = PurchasesUpdatedListener{
-                billingResult, purchases ->
-        }
-
         // Initialize billing client
         billingClient = BillingClient.newBuilder(this)
             .enablePendingPurchases()
-            .setListener(purchaseUpdateListener)
+            .setListener(this)
             .build()
 
-        //start the connection after initializing the billing client
-        establishedConnection()
     }
 
     private fun establishedConnection() {
 
         // Established a connection to google play
-        binding.purchaseProduct.setOnClickListener {
-
-            billingClient?.startConnection(object : BillingClientStateListener {
-                override fun onBillingServiceDisconnected() {
-
-                    // Try to restart the connection
-                    establishedConnection()
-                }
-
-                override fun onBillingSetupFinished(billingResult: BillingResult) {
-                    if (billingResult.responseCode == BillingResponseCode.OK) {
-                        // Query for available products and show them
-                        showProducts()
-                    }
-                }
-
-            }) ?: 0
-        }
+        billingClient?.startConnection(this) ?: 0
     }
 
     private fun showProducts() {
 
         val productList = listOf(
             QueryProductDetailsParams.Product.newBuilder()
-                .setProductId("product_id_example")
+                .setProductId("android.test.purchased")
                 .setProductType(BillingClient.ProductType.INAPP)
                 .build()
         )
@@ -103,18 +77,22 @@ class MainActivity : AppCompatActivity() {
             .build() // pass the reference of product list
 
         // queryProductDetailsAsync
-        billingClient?.queryProductDetailsAsync(queryProductDetailsParams) {
-                billingResult, productDetailsList ->
-
-            // Iterate over each product
-            for (productDetail in productDetailsList) {
-                // show the product details on UI and perform necessary functionality
-                // Boiler or sample code
-                if (productDetail.productId == "product_id_example") {
-                    Log.d("testOffers", "$productDetail")
-                    // Launch Purchase Flow
-                    launchPurchaseFlow(productDetail)
+        billingClient?.queryProductDetailsAsync(queryProductDetailsParams) { billingResult, productDetailsList ->
+            if (billingResult.responseCode == BillingResponseCode.OK) {
+                // Iterate over each product
+                for (productDetail in productDetailsList) {
+                    // show the product details on UI and perform necessary functionality
+                    // Boiler or sample code
+                    if (productDetail.productId == "android.test.purchased") {
+                        Log.d("testOffers", "$productDetail")
+                        // Launch Purchase Flow
+                        binding.purchaseProduct.setOnClickListener {
+                            launchPurchaseFlow(productDetail)
+                        }
+                    }
                 }
+            } else {
+                // Show the error
             }
         } ?: 1
     }
@@ -136,48 +114,60 @@ class MainActivity : AppCompatActivity() {
 
         // launch the billing flow
         // Note: The result of launchBillingFlow is a BillingResult indicating the success or failure of the flow
-        val billingResult = billingClient?.launchBillingFlow(this@MainActivity, billingFlowParams) ?: 3
+        val billingResult =
+            billingClient?.launchBillingFlow(this@MainActivity, billingFlowParams) ?: 3
     }
 
+    override fun onPurchasesUpdated(
+        billingResult: BillingResult,
+        purchases: MutableList<Purchase>?
+    ) {
+        //  handle the purchases
+        if (billingResult.responseCode == BillingResponseCode.OK && purchases != null) {
+            // Iterate over each purchase and handle it
+            for (purchase in purchases) {
+                handlePurchase(purchase)
+            }
+        } else if (billingResult.responseCode == BillingResponseCode.USER_CANCELED) {
+            // Handle an error caused by a user cancelling the purchase flow.
+        } else {
+            // Handle any other error codes.
+        }
+    }
 
-    /*private fun setUpListeners() {
+    private fun handlePurchase(purchase: Purchase) {
 
-        // This code established a connection to google play on button click
-        binding.purchaseProduct.setOnClickListener {
-
-            // Connect to Google Play
-            billingClient?.startConnection(object : BillingClientStateListener {
-                override fun onBillingServiceDisconnected() {
-                    *//*  Try to restart the connection on the next request to Google Play by calling the startConnection() method. *//*
-                }
-
-                override fun onBillingSetupFinished(billingResult: BillingResult) {
-                    if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
-                        // The BillingClient is ready. You can query for purchases and in-app product details.
-                        val params = SkuDetailsParams.newBuilder()
-                        params.setSkusList(skuList)
-                            .setType(BillingClient.SkuType.INAPP)
-
-                        // To query for in-app product details, call queryProductDetailsAsync().
-                        billingClient?.querySkuDetailsAsync(params.build()) {
-                                billingResult, skuDetailsList ->
-
-                            // Launch the purchase flow
-                            for (skuDetails in skuDetailsList!!) {
-                                val purchaseFlow = BillingFlowParams.newBuilder()
-                                    .setSkuDetails(skuDetails)
-                                    .build()
-
-                                // Launch the billing flow
-                                val billingResult = billingClient?.launchBillingFlow(this@MainActivity, purchaseFlow)?.responseCode
-                            }
-
-                        }
+        // handle the purchase here
+        if (purchase.purchaseState == PurchaseState.PURCHASED) {
+            if (!purchase.isAcknowledged) {
+                val acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder()
+                    .setPurchaseToken(purchase.purchaseToken)
+                    .build()
+                val listener = AcknowledgePurchaseResponseListener { billingResult ->
+                    if (billingResult.responseCode == BillingResponseCode.OK) {
+                        // Acknowledgment successful, you can perform additional actions if needed
+                    } else {
+                        // Handle acknowledgment failure
                     }
                 }
-
-            })
+                billingClient?.acknowledgePurchase(acknowledgePurchaseParams, listener)
+            }
         }
+    }
 
-    }*/
+    override fun onBillingServiceDisconnected() {
+        // Retry the connection
+        establishedConnection()
+    }
+
+    override fun onBillingSetupFinished(billingResult: BillingResult) {
+        if (billingResult.responseCode == BillingResponseCode.OK) {
+            // Query for available products and show them
+            showProducts()
+            Log.d(APP_TAG, "Billing setup success. Response code: ${billingResult.responseCode}")
+        } else {
+            Log.d(APP_TAG, "Billing setup Failed. Response code: ${billingResult.responseCode}")
+        }
+    }
+
 }
